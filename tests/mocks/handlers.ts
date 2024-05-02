7e3b0e686e465e34a1d9a18bbcef367660611061
Aurélien Devaux <37188045+aureldvx@users.noreply.github.com>
2024-05-02 17:16:25 +0200

    tests: configure mocks with msw
    Signed-off-by: Aurélien Devaux <37188045+aureldvx@users.noreply.github.com>

---
+++ tests/mocks/handlers.ts
@@ -0,0 +1,30 @@
+import { http, HttpResponse } from 'msw';
+
+import { ApiEndpoints } from '#src/api_endpoints.js';
+import { endpoint, errorHeaders, getMockResponse, successHeaders } from '#tests/mocks/utils.js';
+
+export const handlers = [
+	http.get(endpoint(ApiEndpoints.XML_CLUB_DETAIL), ({ request }) => {
+		const url = new URL(request.url);
+		const club = url.searchParams.get('club');
+
+		if (!club) {
+			return HttpResponse.xml(getMockResponse('bad_request', { parameter: 'club' }), {
+				status: 400,
+				headers: errorHeaders,
+			});
+		}
+
+		if (club !== '10160051') {
+			return HttpResponse.xml(getMockResponse('empty_list', {}), {
+				status: 200,
+				headers: successHeaders,
+			});
+		}
+
+		return HttpResponse.xml(getMockResponse('club_detail', {
+			federalId: club.replace('1', '2'),
+			code: club,
+		}), { status: 200, headers: successHeaders });
+	}),
+];
---
+++ tests/mocks/utils.ts
@@ -0,0 +1,62 @@
+import { readFileSync } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { DateTime } from 'luxon';
+
+const baseUrl = 'https://apiv2.fftt.com/mobile/pxml';
+
+export function endpoint(path: string) {
+	return baseUrl + path;
+}
+
+const defaultHeaders = {
+	'date': DateTime.now().toHTTP(),
+	'server': 'Apache',
+	'cache-control': 'no-cache, private',
+	'cache-version': '8e06150efd0545adee451f9755411c6b',
+	'current-season': '24',
+	'current-season-update': '2023-07-04T10:10:49+02:00',
+	'link': '<http://apiv2.fftt.com/api/docs.jsonld>; rel="http://www.w3.org/ns/hydra/core#apiDocumentation"',
+	'content-security-policy': "frame-ancestors 'self'",
+	'access-control-allow-origin': '*',
+	'access-control-allow-methods': 'GET, POST, OPTIONS, DELETE, PUT',
+	'access-control-allow-headers': 'DNT,authorization,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Cache-Version,Content-disposition,Current-Season,Current-Season-Update',
+	'access-control-expose-headers': 'Content-Length,Content-Range,Cache-Version,Content-disposition,Current-Season,Current-Season-Update',
+	'x-xss-protection': '1; mode=block',
+};
+
+export const errorHeaders = {
+	...defaultHeaders,
+	'content-type': 'application/xml',
+};
+
+export const successHeaders = {
+	...defaultHeaders,
+	'content-type': 'application/xml;charset=ISO-8859-1',
+};
+
+const mockResponses = {
+	empty_list: [],
+	bad_request: ['parameter'],
+	club_detail: ['federalId', 'code'],
+} as const;
+
+type MockResponse = keyof typeof mockResponses;
+
+type MockResponseReplacements<MockName extends MockResponse> = Record<typeof mockResponses[MockName][number], string>;
+
+export function getMockResponse<Response extends MockResponse>(name: Response, replacements: MockResponseReplacements<Response>) {
+	const filePath = resolve(dirname(fileURLToPath(import.meta.url)), `responses/${name}.xml`);
+	const file = readFileSync(filePath, { encoding: 'utf8' });
+
+	if (Object.entries(replacements).length <= 0) return file;
+
+	let result = file;
+
+	for (const [key, value] of Object.entries<string>(replacements)) {
+		result = result.replaceAll(new RegExp(`{{${key}}}`, 'g'), value);
+	}
+
+	return result;
+}
